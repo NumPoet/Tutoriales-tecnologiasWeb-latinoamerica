624e5d5f40c270b1e37cb184930a43ffbdc5b1ea
Sergio Negrete Villegas (numpoet) <bonekob2rt@gmail.com>
2018-10-13 18:43:45 -0500

    express: rutas, configuracion, plantillas html con ejs

--- primer-server-express/index.js
+++ primer-server-express/index.js
@@ -1,10 +1,30 @@
 const express = require ('express');
+const morgan = require('morgan');
 const app = express();
 
+// Configuraciones
+
+app.set('appName', 'Mi primer server en express');
+app.set('views', __dirname+'/views');
+app.set('View engine', 'ejs');
+// Middlewares
+app.use(morgan('combined'))
+
+
+// Rutas
 app.get('/', (req, res) => {
-    res.end('Hola Fuckyng Mundo!!!!!!!!');
+    res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.end('Aquí va  el login');
+});
+
+app.get('*', (req, res) =>{
+    res.end('Archivo no encontrado');
 });
 
 app.listen(3000, function () {
     console.log('Servidor Funcionando!!!!');
+    console.log('Nombre de la App: '+app.get('appName'));
 });
